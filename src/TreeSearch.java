643329ead37d4b3d75c7a3b4722c9a6405b7ab95
Alla <zhalla1999@mail.ru>
2019-09-11 23:18:45 +0300

    Создан список того, что надо сделать

--- src/TreeSearch.java
+++ src/TreeSearch.java
@@ -1,4 +1,14 @@
+import java.util.Queue;
+
 public class TreeSearch
 {
+    /*
+    TODO добавить:
+                    метод с поиском
+                    метод добавления элементов в очередь
+     */
+    Queue<Node> queue;
+    Node root = new Node();
+    Node answer;
 
 }
