d1fc057423051bd0c81742ddb425f6528fbceac3
Alla <zhalla1999@mail.ru>
2019-09-11 21:05:03 +0300

    Добавлены операции сдвига

--- src/Node.java
+++ src/Node.java
@@ -3,7 +3,7 @@
 class Node
 {
     /**Состояние*/
-    private int [][] state = new int [3][3];
+    private int [][] state;
 
     /**Родитель вершины*/
     private Node parent;
@@ -31,33 +31,75 @@ class Node
         return new int [] {-1,-1};
     }
 
+    /**Конструктор для создания корня дерева*/
+    Node()
+    {
+        state = new int [][] {{4,8,1},{0,3,6},{2,7,5}};
+        parent = null;
+        action = null;
+        cost = 0;
+        depth = 0;
+    }
+
     /**Конструктор
      * @param _parent - родитель узла
-     * @param _action - операция, изменяющая состояние*/
-    Node(Node _parent, Action _action)
+     * @param _action - операция, изменяющая состояние
+     * @param i - строка, в которой находится пустая клетка
+     * @param j - столбец, в котором находится пустая клетка*/
+    Node(Node _parent, Action _action, int i, int j)
     {
         parent = _parent;
         action = _action;
         depth = _parent.depth+1;
         cost = _parent.cost+1;
-        int[] empt = _parent.findEmptyPlace();
+        state = _parent.state;
         if(action == Action.UP)
-        {
-            System.out.println("UP");
-        }
+            this.action_UP(i,j);
         if(action == Action.DOWN)
-        {
-            System.out.println("DOWN");
-        }
+            this.action_DOWN(i,j);
         if(action == Action.LEFT)
-        {
-            System.out.println("LEFT");
-        }
+            this.action_LEFT(i,j);
         if(action == Action.RIGHT)
-        {
-            System.out.println("RIGHT");
-        }
+            this.action_RIGHT(i,j);
+    }
 
+    /**Выполнение операции "Вверх"
+     * @param i - строка, в которой находится пустая клетка
+     * @param j - столбец, в котором находится пустая клетка*/
+    private void action_UP(int i, int j)
+    {
+        System.out.println("UP");
+        state[i][j] = state[i+1][j];
+        state[i+1][j] = 0;
+    }
 
+    /**Выполнение операции "Вниз"
+     * @param i - строка, в которой находится пустая клетка
+     * @param j - столбец, в котором находится пустая клетка*/
+    private void action_DOWN(int i, int j)
+    {
+        System.out.println("DOWN");
+        state[i][j] = state[i-1][j];
+        state[i-1][j] = 0;
+    }
+
+    /**Выполнение операции "Влево"
+     * @param i - строка, в которой находится пустая клетка
+     * @param j - столбец, в котором находится пустая клетка*/
+    private void action_LEFT(int i, int j)
+    {
+        System.out.println("LEFT");
+        state[i][j] = state[i][j+1];
+        state[i][j+1] = 0;
+    }
+
+    /**Выполнение операции "Вправо"
+     * @param i - строка, в которой находится пустая клетка
+     * @param j - столбец, в котором находится пустая клетка*/
+    private void action_RIGHT(int i, int j)
+    {
+        System.out.println("RIGHT");
+        state[i][j] = state[i][j-1];
+        state[i][j-1] = 0;
     }
 }
